9bf3459aab5da46020ceb296743e863d8b610a5e
shivam <shivam@gmail.com>
2024-03-16 15:06:49 +0530

    push

---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+//temporary souls firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBe1Uakt2inmTXf2ppgknJ5tCom7pT22UY",
+  authDomain: "tour-and-travel-6699d.firebaseapp.com",
+  projectId: "tour-and-travel-6699d",
+  storageBucket: "tour-and-travel-6699d.appspot.com",
+  messagingSenderId: "616528203207",
+  appId: "1:616528203207:web:a68ad17a2fb2631864dde2",
+  measurementId: "G-5K42V3GK1R"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
