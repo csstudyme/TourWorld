80f2ac5e90cde93dcb788f8daf2de3ac8a357bc2
Ajay <ajaypanpatil01@gmail.com>
2024-04-21 01:18:18 +0530

    Addd files

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -311,7 +311,7 @@ const AdminDashboard = () => {
                   >
                     Payments
                   </button>
-                  {/* <button
+                   <button
                     className={
                       activePanelId === 6
                         ? "p-1 rounded-t transition-all duration-300 text-nowrap bg-blue-500 text-white"
@@ -321,7 +321,7 @@ const AdminDashboard = () => {
                     onClick={() => setActivePanelId(6)}
                   >
                     Ratings/Reviews
-                  </button> */}
+                  </button>
                   {/* <button
                     className={
                       activePanelId === 7
